c89fd41aa3460d370b8f255ba142b4a7a7e62104
Nikola Mihajlovic <NikolaMihajlovic@192.168.1.7>
2022-07-06 13:34:25 +0200

    added changes as per agreement

--- __tests__/api/api_spec.js
+++ __tests__/api/api_spec.js
@@ -1,21 +1,30 @@
 const frisby = require('frisby');
+const url =  'https://simple-books-api.glitch.me';
 
-it('ApiStatus', function () {
-    return frisby.get('https://simple-books-api.glitch.me/status')
+it('ApiStatus', function() {
+    return frisby.get(url + '/status') 
+      .expect('status', 200);
+  });
+  
+it('ListOfBooks', function() {
+    return frisby.get(url + '/books')
       .expect('status', 200);
   });
 
-it('ListOfBooks', function () {
-    return frisby.get('https://simple-books-api.glitch.me/books')
-      .expect('status', 200);
-  });
-
-it('OrderBook', function () {
-    return frisby.get('https://simple-books-api.glitch.me/orders')
+  it('Valid response status', function () {
+    return frisby.post(url + '/orders' , {
+      headers: {
+        'Authorization' : 'd79e77fcf5d1563fe03512aa1d3376e667f0c737781e50fe449ae1a5cc4561d2'
+      },
+      body: {
+        "bookId": 1,
+        "customerName": "Ms. Christie Rempel"
+      }
+    })
       .expect('status', 201);
   });
 
-it('ReqNameApiClient', function () {
-    return frisby.get('https://simple-books-api.glitch.me/api-client')
+it('ReqNameApiClient', function() {
+    return frisby.get(url + '/api-client')
       .expect('status', 404);
   });
